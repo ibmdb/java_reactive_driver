08e752a68b59a3b399f5c17eae28d704bfbb15dc
rappavu <rappavu@host.docker.internal>
2023-05-20 21:04:52 +0530

    Updated run_tests script to run specific a test suite.

--- bin/win/run_tests.bat
+++ bin/win/run_tests.bat
@@ -3,4 +3,12 @@
 copy %INSTALL_DIR%\config\* %INSTALL_DIR%\classes\.
 copy %INSTALL_DIR%\tests\resources\* %INSTALL_DIR%\classes\.
 
+if %1. == . goto runall
+
+java -cp "%cp%;resources" -Dfile.encoding=UTF8 org.junit.runner.JUnitCore %1
+goto done
+
+:runall
 java -cp "%cp%;resources" -Dfile.encoding=UTF8 org.junit.runner.JUnitCore com.ibm.db2.r2dbc.TestSuite
+
+:done
